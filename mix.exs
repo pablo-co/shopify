eb89b41903b9aaadd23da51bb857b20057e28b46
Pablo Cárdenas <pcardenasoliveros@gmail.com>
2017-04-05 18:38:00 -0500

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -51,8 +51,8 @@ defmodule Shopify.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.10.0"},
-      {:poison, "~> 3.0"},
+      {:httpoison, ">= 0.11.0"},
+      {:poison, ">= 1.5.0"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
